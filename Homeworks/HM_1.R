319ef3c00b8799b92794ccc573fc53972d67157a
rafrys <rafal.rysiejko@gmail.com>
2020-02-23 16:26:35 +0100

    After changes

--- Homeworks/HM_1.R
+++ Homeworks/HM_1.R
@@ -37,16 +37,13 @@ head(diamonds)
 
 data.list <- split(diamonds, interaction(diamonds$cut, diamonds$color))
 
-models <- lapply(varlist, function(x) {
-  lm(price ~ carat, data=data.frame(price=data.list[[x]][7], carat=data.list[[x]][1]))
-})
-
-
 # a. for each element perform a regression: price ~ carat. Save the results in the list
 
+varlist <- names(data.list)
 models <- lapply(varlist, function(x) {
   lm(price ~ carat, data=data.frame(price=data.list[[x]][7], carat=data.list[[x]][1]))
 })
+
 
 # b. for each model generate summary and save result in another lists.
 summaries <- lapply(models, summary)
@@ -66,19 +63,24 @@ Height <- rnorm(100, mean=170, sd=10)
 Woman <- factor(floor(2*runif(100)))
 d <- data.frame(Woman, Height)
 
-condiitonal_summary <- function(data, sex){
-  pastecs::stat.desc(d$Height[d$Woman == sex])[c(9, 8, 12, 13)]
+condiitonal_summary <- function(df, sex){
+  pastecs::stat.desc(df$Height[df$Woman == sex])[c(9, 8, 12, 13)]
   }
 
 condiitonal_summary(d,1) # for women
 condiitonal_summary(d,0) # for men
 
 # 4b. Use different function and repeat exercises for each Species in iris dataset.
-for (i in unique(iris$Species)){
-  print(str_interp('Summary for: ${i}'))
-  print(round(pastecs::stat.desc(iris[iris$Species == i,])[c(9, 8, 12, 13),c(1:4)],2))
-  cat("\n")
+
+conditional_summary_general <- function(df,factor_variable){
+  for (i in unique(eval(substitute(factor_variable), df))){
+    print(str_interp('Summary for: ${i}'))
+    print(round(pastecs::stat.desc(iris[iris$Species == i, ])[c(9, 8, 12, 13), c(1:4)], 2))
+    cat("\n")
+  }
 }
+
+conditional_summary_general(iris,Species)
 
 # 5. Generate a list of 100 vectors: c(1), c(1,2), c(1,2,3), ... , c(1,...,100)
 list <- lapply(1:100, seq)
