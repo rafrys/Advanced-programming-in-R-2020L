bcbb6106a69d100912955a745f9786a614405199
rafrys <rafal.rysiejko@gmail.com>
2020-05-14 09:15:00 +0200

    11HM

--- Homeworks/HM_10/HM_10/app.R
+++ Homeworks/HM_10/HM_10/app.R
@@ -9,6 +9,7 @@ library(shiny)
 library(DT)
 library(ggplot2)
 library(shinythemes)
+library(tidyverse)
 
 mydata = read.csv("mtcars.csv", header = T,row.names =NULL)
 
@@ -16,13 +17,19 @@ attach(mydata)
 
 server = function(input, output, session){
     
-    # table for the Data table tab
+    mydata_filtered <- reactive({
+        mydata %>%
+            filter(
+                between(disp, input$displacement_var[1], input$displacement_var[2]),
+                cyl %in% input$cylinders)
+    })
+
     
-    output$tableDT <- DT::renderDataTable(DT::datatable(mydata))
+    output$tableDT <- DT::renderDataTable(DT::datatable(mydata_filtered()))
     
     
     output$scat = renderPlot({
-        ggplot(mydata, aes(mpg, disp)) +
+        ggplot(mydata_filtered(), aes(mpg, disp)) +
             geom_point() + geom_smooth(method = "lm") +
             xlab("Miles/(US) gallon") + ylab("Displacement (cu.in.)")
     })
@@ -31,7 +38,7 @@ server = function(input, output, session){
     mydata.new = reactive({
         
         user_brush <- input$user_brush
-        mysel <- brushedPoints(mydata, user_brush)
+        mysel <- brushedPoints(mydata_filtered(), user_brush)
         return(mysel)
         
     })
@@ -46,12 +53,25 @@ server = function(input, output, session){
     
     
 }
+###########
+### UI ###
 
 ui = navbarPage(theme = shinytheme("sandstone"), title = h3("MTCars dataset analysis"),
                 tabPanel(
                     ("Miles per gallon vs. diesplacement"),
-                    
-                    plotOutput("scat", brush = "user_brush"),
+                    sidebarLayout(
+                        sidebarPanel(
+                            selectInput("cylinders", "Cylinders - Multiselect",
+                                        c("Four" = 4,
+                                          "Six" = 6,
+                                          "Eight" = 8),multiple = T),
+                            sliderInput("displacement_var", HTML("Displacement [CU<sup>2</sup>]:"),
+                                        min = 100, max = 360, value = c(100, 300), step = 10)
+                        ),
+                        mainPanel(
+                            plotOutput("scat", brush = "user_brush")
+                        )
+                    ),
                     DT::dataTableOutput("table"),
                     downloadButton(outputId = "mydownload", label = "Download Table")
                 ),
@@ -59,17 +79,21 @@ ui = navbarPage(theme = shinytheme("sandstone"), title = h3("MTCars dataset anal
                 tabPanel("Documentation",
                          h4("Project Functionalities:"),
                          tags$div(
-                             "1. Visualize the relationship between miles per gallon and displacement of the cars in the MTcars dataset.", 
+                             "1. (Multi) select the number of cylinders (four,six,eight) of the cars in the MTcars dataset..", 
                              tags$br(),
-                             "2. Select observations on a graph using brush feature from the DataTables shiny package.",
+                             "2. Select the range of engine displacement by using a slider.", 
                              tags$br(),
-                             "3. Display selected observations in a data table.",
+                             "3. Visualize the relationship between miles per gallon and displacement.", 
                              tags$br(),
-                             "4. Filter record in a data table via theSearch Field.",
+                             "4. Select observations on a graph using brush feature from the DataTables shiny package.",
                              tags$br(),
-                             "5. Move to the 'Previous' or 'Next' tab of data table",
+                             "5. Display selected observations in a data table.",
                              tags$br(),
-                             "6. Download selected observations to .csv",
+                             "6. Filter record in a data table via theSearch Field.",
+                             tags$br(),
+                             "7. Move to the 'Previous' or 'Next' tab of data table",
+                             tags$br(),
+                             "8. Download selected observations to .csv",
                              
                          )
                          
